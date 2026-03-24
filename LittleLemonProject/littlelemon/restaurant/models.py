bc4d0ea2d8788d53c74cdc1e486a84132484dc06
unknown <tekorotekoro@gmail.com>
2026-03-24 18:47:20 +0100

    Setting up table booking API

--- LittleLemonProject/littlelemon/restaurant/models.py
+++ LittleLemonProject/littlelemon/restaurant/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 class Booking(models.Model):
     name = models.CharField(max_length=255)
     no_of_guests = models.IntegerField()
-    booking_date = models.DateTimeField()
+    booking_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"{self.name} - {self.booking_date}"
