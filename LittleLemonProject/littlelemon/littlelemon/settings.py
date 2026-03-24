7223ad58be82d198271fcf2a2c66469c21666a92
unknown <tekorotekoro@gmail.com>
2026-03-24 19:45:51 +0100

    Update on setting up table booking API

--- LittleLemonProject/littlelemon/littlelemon/settings.py
+++ LittleLemonProject/littlelemon/littlelemon/settings.py
@@ -128,10 +128,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': [
-        # 'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
-    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
